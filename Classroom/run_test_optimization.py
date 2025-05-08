930d4d4bd54b19c40df932183880d7ebba931d6f
Lucas Van der Hauwaert <lucasvdhauwaert@gmail.com>
2025-05-08 11:35:21 +0200

    fixed install bugs (requirements.txt)

--- Classroom/run_test_optimization.py
+++ Classroom/run_test_optimization.py
@@ -6,7 +6,7 @@ from outdoor.outdoor_core.input_classes.superstructure import Superstructure
 # get current working directory
 current_path = os.getcwd()
 # add the file name Aurelie_peptide_production_superstructure.pkl to the current working directory
-path = os.path.join(current_path, "TP_case_study_superstructure.pkl") # TP_case_study_superstructure  or tp_test_superstructure
+path = os.path.join(current_path, "TP_case_study_superstructure_test.pkl") # TP_case_study_superstructure  or tp_test_superstructure
 with open(path, 'rb') as file:
     superstructureObj = pickle.load(file)
 
