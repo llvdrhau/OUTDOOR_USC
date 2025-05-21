1c368fe2ec6c7138c9e423dbae8eb263b5497009
Lucas Van der Hauwaert <lucasvdhauwaert@gmail.com>
2025-05-21 16:13:59 +0200

    install Brightway script, patches

--- Classroom/install_brightway.py
+++ Classroom/install_brightway.py
@@ -3,14 +3,16 @@ import bw2io as bi
 
 if "outdoor" not in bd.projects:
     bd.projects.create_project("outdoor")
-
     bd.projects.set_current("outdoor")
-    bi.import_ecoinvent_release(version="3.9.1", system_model="consequential",
-                                username="ACV_IIT",password="Ecoinvent123")
+    try:
+        bi.import_ecoinvent_release(version="3.9.1", system_model="consequential",
+                                     username="ACV_IIT",password="Ecoinvent123")
+        # This prints the directory where the current project is stored
+        print(bd.projects.dir)
+        print(bd.__version__)
 
-    #This prints the directory where the current project is stored
-    print(bd.projects.dir)
-    print(bd.__version__)
+    except Exception as e:
+        print("The following error occurred:",e)
 
 else:
     # print in green
