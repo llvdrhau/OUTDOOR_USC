270ef1f604bc32bb436245c58081960f16cff472
Lucas Van der Hauwaert <lucasvdhauwaert@gmail.com>
2025-05-21 15:52:27 +0200

    install Brightway script made

---
+++ Classroom/install_brightway.py
@@ -0,0 +1,17 @@
+import bw2data as bd
+import bw2io as bi
+
+if "outdoor" not in bd.projects:
+    bd.projects.create_project("outdoor")
+
+    bd.projects.set_current("outdoor")
+    bi.import_ecoinvent_release(version="3.9.1", system_model="consequential",
+                                username="ACV_IIT",password="Ecoinvent123")
+
+    #This prints the directory where the current project is stored
+    print(bd.projects.dir)
+    print(bd.__version__)
+
+else:
+    # print in green
+    print("\033[92m" + "Brightway already installed" + "\033[0m")
